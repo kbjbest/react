217e6eca98428f6184269d9af556e349fc075ec5
Beomju Kim <kbj7748@naver.com>
2022-11-04 10:28:35 +0900

    2022.11.03

---
+++ fiveProject/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+function App() {
+  console.log("App!!!");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    console.log("useEffect!!!");
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    console.log("storedUserLoggedInInformation : " + storedUserLoggedInInformation);
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    console.log("loginHandler!!!");
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    console.log("logoutHandler!!!");
+    localStorage.removeItem("isLoggedIn", "0");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
